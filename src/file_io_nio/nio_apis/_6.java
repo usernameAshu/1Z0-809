5d60b0f4d2e8de7227e712709f381481892c2427
Ashutosh Mohanty <mohantyashu.07@gmail.com>
2023-06-19 01:58:39 +0530

    change

--- src/file_io_nio/nio_apis/_6.java
+++ src/file_io_nio/nio_apis/_6.java
@@ -15,13 +15,16 @@ public class _6 {
 	public static void main(String[] args) throws IOException {
 		
 		Path p1 = Paths.get("demo1.txt");
-		Path p2 = Paths.get("demo2.txt");
+		Path p2 = Paths.get("nio/nio1/demo2");
+		Path file_p2 = Paths.get("nio/nio1/demo2", "nio_file.txt");
 		
 		Files.createFile(p1);
-		Files.createFile(p2);
+		Files.createDirectories(p2);
+		Files.createFile(file_p2);
 		
 		boolean result = Files.isSameFile(p1, p2);
 		System.out.println(result);
+
 	}
 
 }
